34e98ab5696459e16a481225c4c65e2199f1545a
ricardojes0412 <ricardomunoz04121998@gmail.com>
2022-01-31 22:14:09 -0500

    add ws

---
+++ gizlo-ws/src/main/java/com/gizlo/GizloWsApplication.java
@@ -0,0 +1,13 @@
+package com.gizlo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GizloWsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GizloWsApplication.class, args);
+    }
+
+}
---
+++ gizlo-ws/src/test/java/com/gizlo/GizloWsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gizlo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GizloWsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
